cc7662db188aaa2622017b2e1f4e6269c55156e4
philipwerner <philip.r.werner@gmail.com>
2018-04-04 10:42:32 -0700

    Completed tutorial

---
+++ Lab12MovieApp/Lab12MovieApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lab12MovieApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
